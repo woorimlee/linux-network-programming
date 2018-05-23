478b9e2451b73c62a857cf5efc4e7ee724a79496
woorimlee <and_lamyland@naver.com>
2018-05-23 06:41:04 +0000

    Sending HTTP Request to the address 'www.google.com' using Socket

--- Sending_HTTP_Request/WebClient.c
+++ Sending_HTTP_Request/WebClient.c
@@ -8,28 +8,28 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-const char * REQUEST_MESSAGE[] = {"",}
+#define BUF_SIZE 1024
 
 int main (int argc, char *argv[]) {
     int sock = 0;
     int port = 0;
-    char *url_Addr;
+    char *url_Addr = "www.google.com";
+    char message[BUF_SIZE] = {0};
     struct sockaddr_in web_Client;
     struct in_addr addr;
     struct hostent *host;    
 
-    if (3 != argc) {
-	fprintf(stderr, "Usage : %s <URL> <Port>\n", argv[0]);
-	exit(2);
+    if (2 != argc) {
+	fprintf(stderr, "Usage : %s <Port>\n", argv[0]);
+	exit(1);
     }
-    
-    url_Addr = argv[1];
-    port = atoi(argv[2]);
+
+    port = atoi(argv[1]);
 
     sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(sock == -1) {
 	fprintf(stderr, "Couldn't create a socket\n");
-	exit(3);
+	exit(2);
     } else fprintf(stderr, "Socket is created\n");
 
     host = gethostbyname(url_Addr);
@@ -43,11 +43,17 @@ int main (int argc, char *argv[]) {
     if(connect(sock, (struct sockaddr *)&web_Client, sizeof(web_Client)) == -1) {
 	fprintf(stderr, "Couldn't connect!\n");
 	close(sock);
-	exit(4);
+	exit(3);
     } else fprintf(stderr, "Connection success\n");
 
-    if(send(sock, ) > 0) {
-	printf("HTTP Requust succeuss using socket\n:);
+    if(send(sock, "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n", sizeof("GET / HTTP/1.1\r\nHost: cote.kw.ac.kr\r\n\r\n"), 0) > 0) {
+	printf("HTTP Requust success using socket\n");
+    }
+    
+    if(recv(sock, message, BUF_SIZE, 0) > 0) {
+	printf("Receive success!!!\n\n========================\n");
+	printf("%s\n",message);
+    }
 
     close(sock);
     return 0;
